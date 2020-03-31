9aca9b43c178a2866ae768cd1901de06a07cc4b3
Jacob Whisler <jwhisler521@gmail.com>
2020-03-30 20:42:23 -0400

    added server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json)
+app.use(express.static('public'))
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
