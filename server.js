25f35b832714eba3f517c75068ac343111ad9e66
Jacob Whisler <jwhisler521@gmail.com>
2020-03-30 21:14:49 -0400

    html routes

--- server.js
+++ server.js
@@ -17,3 +17,8 @@ mongoose.connect(MONGODB_URI, {
     useFindAndModify: false
 })
 
+require('./routes/htmlroutes')(app)
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
