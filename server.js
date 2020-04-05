b0ce9e61ef6e1c39244f066a8aa2de9f52c5581c
Jacob Whisler <jwhisler521@gmail.com>
2020-04-04 23:47:50 -0400

    server

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const morgan = require('morgan')
-const mongoose = require('mongoose')
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan('dev'))
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json)
-app.use(express.static('public'))
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ useNewUrlParser: true, useUnifiedTopology: true });
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+const db = require("./models");
 
-require('./routes/htmlroutes')(app)
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on ${PORT}`);
+});
